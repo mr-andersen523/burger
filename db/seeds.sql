45751693f7984f35ada3b3960b0584d7cda3ae74
mr andersen <chasea523@gmail.com>
2020-01-30 16:45:19 -0800

    queries to populate db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Greasy Lard Bomb", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Bacon Grenade Deluxe", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Paunch Burger", false);
