9dd5aaaf1b7fc93c4c370f3351494c1cadf91764
mr andersen <chasea523@gmail.com>
2020-01-30 16:55:59 -0800

    added skeleton for orm

---
+++ db/config/orm.js
@@ -0,0 +1,22 @@
+const connection = require("../config/connection.js");
+
+const orm = {
+
+    //METHODS//
+
+    selectAll()
+
+
+    insertOne()
+
+
+    updateOne()
+
+
+}
+
+
+
+
+// export ORM object
+module.exports = orm;
