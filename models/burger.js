7cda246e08cc88c0db142aacae3fbacdeffe0ef0
mr andersen <chasea523@gmail.com>
2020-01-30 17:01:19 -0800

    added skeleton for burgers.js

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+// import `orm.js` 
+const orm = require("../config/orm.js");
+
+// TODO: code that will call the ORM functions using burger specific input for the ORM
+const burgers = {
+
+
+
+
+
+
+
+}
+
+
+
+
+
+// Export at the end of the `burger.js` file
+module.exports = burgers;
