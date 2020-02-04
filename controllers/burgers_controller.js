8da7d444e77697087dba3250fa01b11e25ca5ded
mr andersen <chasea523@gmail.com>
2020-02-03 18:05:16 -0800

    orm methods

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,19 +2,48 @@ const express = require("express");
 const burgers = require("../models/burgers");
 const router = express.Router();
 
+// GET route - Receive all burgers from db
+router.get("/", (req, res) => {
+
+    // returns all burgers from database to index.handlebars
+    burgers.selectAll((data) => {
+        return res.render("index", { hamburgers: data });
+      });
+  });
 
 
 
+  // POST route - Add new burger to db
+router.post("/api/hamburger", (req, res) => {
+
+    // Inserts hamburger to db
+  burgers.insert(req.body.name, false, (result) => {
+    // Returns response for ajax call
+    if (result.changedRows == 1) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 
+// PUT Route - Updates hamburger to devoured
+router.put("/api/hamburger/:id", (req, res) => {
 
-
-
-
-
-
-
+    const id = req.params.id;
+    
+      // Update hamburger in db
+  burgers.update(id, true, (result) => {
+    // Returns response for ajax call
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,33 @@
 // import `orm.js` 
 const orm = require("../config/orm.js");
 
-// TODO: code that will call the ORM functions using burger specific input for the ORM
+// code that will call the ORM functions using burger specific input for the ORM
+// Burger Model
 const burgers = {
 
+    // SelectAll method to call orm.selectAll
+    selectAll: (callback) => {
 
+        orm.selectAll("burgers", (res) => {
+            callback(res);
+        });
+    },
 
+    // Insert method to call orm.insert
+    insert: (burgerName, devoured, callback) => {
 
+        orm.insertOne("burgers", "burger_name", "devoured", burgerName, devoured, (res) => {
+            callback(res);
+        });
+    },
 
+    // Update method to call orm.updateOne
+    update: (id, devoured, callback) => {
+        orm.updateOne("burgers", "devoured", devoured, "id", id, (res) => {
+            callback(res);
+        });
+    },
 
-
-}
-
-
-
-
-
+};
 // Export at the end of the `burger.js` file
 module.exports = burgers;
