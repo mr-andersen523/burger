49bfac211a685d88c868f9c509901879588397ed
mr andersen <chasea523@gmail.com>
2020-02-04 23:16:46 -0800

    minor changes

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   burger_name varchar(100) NOT NULL,
-  devoured BOOLEAN,
+  devoured BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
 );
